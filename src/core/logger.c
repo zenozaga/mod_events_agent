9f8c6fd6e50d3430dda98911bf9512434a86ac3e
Randy stiven Valentin <zenozaga@gmail.com>
2026-01-05 12:01:07 -0400

    Ensure core sources tracked and adjust gitignore

---
+++ src/core/logger.c
@@ -0,0 +1,15 @@
+#include "mod_event_agent.h"
+#include <stdarg.h>
+
+void event_agent_log(switch_log_level_t level, const char *fmt, ...)
+{
+    char *log_line;
+    va_list ap;
+
+    va_start(ap, fmt);
+    if (switch_vasprintf(&log_line, fmt, ap) != -1) {
+        switch_log_printf(SWITCH_CHANNEL_LOG, level, "[mod_event_agent] %s\n", log_line);
+        free(log_line);
+    }
+    va_end(ap);
+}
